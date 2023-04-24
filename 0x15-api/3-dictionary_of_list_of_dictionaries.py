52209fcba92c8adcdeff577018cbaf8e2ede0ab9
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-24 21:54:12 +0300

    dictionary_of_list_of_dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,29 +9,29 @@ import sys
 
 
 if __name__ == '__main__':
-    # Get all employees
+    """ Get all employees"""
     employees_url = 'https://jsonplaceholder.typicode.com/users'
     employees = requests.get(employees_url).json()
 
-    # Initialize a dictionary to store tasks by employee
+    """ Initialize a dictionary to store tasks by employee"""
     tasks_by_employee = {}
 
-    # Get tasks for each employee
+    """Get tasks for each employee"""
     for employee in employees:
-        employee_id = employee['id']
-        tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        tasks = requests.get(tasks_url).json()
+        emp_id = employee['id']
+        tsk_url = f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}'
+        tasks = requests.get(tsk_url).json()
 
-        # Add tasks to the dictionary
-        tasks_by_employee[employee_id] = []
+        """ Add tasks to the dictionary """
+        tasks_by_employee[emp_id] = []
         for task in tasks:
             task_dict = {
                 'username': employee['username'],
                 'task': task['title'],
                 'completed': task['completed']
             }
-            tasks_by_employee[employee_id].append(task_dict)
+            tasks_by_employee[emp_id].append(task_dict)
 
-    # Export tasks to a JSON file
+    """ Export tasks to a JSON file """
     with open('todo_all_employees.json', mode='w') as json_file:
         json.dump(tasks_by_employee, json_file)
