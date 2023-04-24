df11c1b22f0683c6afba98e4a13a4af4547b3737
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-24 20:15:32 +0300

    init

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,68 @@
 #!/usr/bin/python3
-    """Gather data from API"""
+"""Gather data from API"""
+    
 import json
 import requests
-from sys import argv
+import sys
+
+def get_employee_info(employee_id):
+    """
+    Given an employee ID, retrieves the employee's name and the number of tasks completed
+    out of the total number of tasks.
+    """
+    # Make a GET request to the API to retrieve the employee data
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+
+    # If the response is not successful, raise an exception
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve employee data: {response.content}")
+
+    # Parse the JSON response to get the employee's name
+    employee_name = response.json()["name"]
+
+    # Make another GET request to retrieve the employee's TODO list
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(url)
+
+    # If the response is not successful, raise an exception
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve employee TODO list: {response.content}")
+
+    # Count the number of completed tasks and the total number of tasks
+    tasks = response.json()
+    total_tasks = len(tasks)
+    completed_tasks = len([task for task in tasks if task["completed"]])
+
+    return employee_name, completed_tasks, total_tasks
+
+def print_employee_todo_list(employee_id):
+    """
+    Given an employee ID, retrieves and prints the employee's TODO list progress.
+    """
+    employee_name, completed_tasks, total_tasks = get_employee_info(employee_id)
+
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+
+    # Make another GET request to retrieve the employee's TODO list
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(url)
+
+    # If the response is not successful, raise an exception
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve employee TODO list: {response.content}")
+
+    # Print the title of each completed task
+    tasks = response.json()
+    for task in tasks:
+        if task["completed"]:
+            print("\t " + task["title"])
 
 
-if __name__ == '__main__':
-    if len(argv) == 2 and argv[1].isdigit():
-        user_id = argv[1]
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-        response = requests.get(url)
-        if response.status_code == 200:
-            user_info = json.loads(response.text)
-            name = user_info.get('name')
-            url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
-            response = requests.get(url)
-            if response.status_code == 200:
-                tasks = json.loads(response.text)
-                total_tasks = len(tasks)
-                completed_tasks = [task.get('title') for task in tasks if task.get('completed')]
-                print("Employee {} is done with tasks({}/{}):".format(name, len(completed_tasks), total_tasks))
-                for task in completed_tasks:
-                    print("\t {}".format(task))
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    print_employee_todo_list(employee_id)
