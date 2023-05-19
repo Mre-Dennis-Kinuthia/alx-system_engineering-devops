05b4a2e5df098da212c5a2194a071b8859abe379
MMre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-05-19 18:18:06 +0300

    Fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Change file limit for nonlogin user.
+exec { 'file_limit':
+  command  => "sed -i 's/15/4096/' /etc/default/nginx",
+    provider => 'shell'
+    }
+
+exec { 'restart_NGINX':
+  command  => 'sudo service nginx restart',
+    provider => 'shell',
+      require  => Exec['file_limit']
+      }
