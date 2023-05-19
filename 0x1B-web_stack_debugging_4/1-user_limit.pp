e122a891d578a8ebe13927c7d3e0efa3f774af9a
MMre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-05-19 19:10:19 +0300

    init su-holberton

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,17 @@
 # OS configuration so its possible to login with the holberton user and open a file without any error
-exec { 'change-os-configuration-for-holberton-user':
-command  => "sed -i 's/4096/16384/' /etc/security/limits.conf",
-provider => 'shell',
+user { 'holberton':
+  ensure => present,
+}
+
+file { '/etc/security/limits.conf':
+  content => "holberton hard nofile 4096\n",
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+}
+
+exec { 'reload_pam_limits':
+  command     => '/usr/sbin/pam-auth-update --package',
+  path        => '/usr/sbin',
+  refreshonly => true,
 }
